f301471292ce6d08bda3efe59f758ab8e40ea559
David Morales <morgar.creative@gmail.com>
2017-12-02 17:53:54 -0600

    fixed bugs in variables, and twitter function

--- liri.js
+++ liri.js
@@ -1,50 +1,41 @@
-var require = require("./keys.js");
+var keys = require("./keys.js");
 var Twitter = require("twitter");
-var Spotify = require("node-spotify-api");
-
+var Spotify = require("spotify-web-api-node");
 var input = process.argv[2];
-
 var twitterAPI = new Twitter(keys.twitter);
 var spotifyAPI = new Spotify(keys.spotify);
 
-function activity (command) {
-  var nodeArg = processArg(process.argv);
-  appendLog(command + " " + nodeArg);
-  switch (input) {
-    case "my-tweets":
-      twitter();
-      break;
 
-    case "spotify-this-song":
-      spotify();
-      break;
-
-    case "movie-this":
-      movie();
-      break;
-
-    case "do-what-it-says":
-      loadFile();
-      break;
-  };
-}
-
-activity(input);
-
-// TWITTER FUNCTION ================================================
-function twitter(){
-
-  var client = new Twitter(keys.twitter);
-  var params = {screen_name: 'nodejs'};
-
-  client.get('statuses/user_timeline', params, function(error, tweets, response) {
-  if (!error) {
-    for (var i = 0; i < tweets.length; i++){
-      console.log(tweet.text + " " + tweet.created_at + " by " + tweet.user.screen_name);
+function activity(command) {
+    switch (input) {
+        case "my-tweets":
+            twitter();
+            break;
+        case "spotify-this-song":
+            spotify();
+            break;
+        case "movie-this":
+            movie();
+            break;
+        case "do-what-it-says":
+            loadFile();
+            break;
     }
-  }
-
-});
-
-
+}
+activity(input);
+// TWITTER FUNCTION ================================================
+function twitter() {
+    var client = new Twitter(keys.twitterKeys);
+    var params = {screen_name: 'nodejs', limit: 1};
+    client.get('statuses/user_timeline', params, function (error, tweets, response) {
+        if (!error) {
+            for (var i = 0; i < tweets.length; i++) {
+                console.log("=============================================");
+                console.log("==============NEW TWEETS=====================");
+                console.log("=============================================");
+                console.log(tweets[i].created_at);
+                console.log(tweets[i].text);
+            }
+        }
+    });
 }
